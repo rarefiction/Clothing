3c9c6b33cd8a4142a1b697ea4f63fa3a651a4e38
rare <nanun93@hotmail.com>
2021-05-22 14:07:01 +1200

    updated shopdater with new data + updated cart utils to group items in cart reducer

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,7 +1,10 @@
 import CartActionTypes from "./cart.types";
 
-const toggleCartHidden = () => ({
+export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDDEN,
 });
 
-export default toggleCartHidden;
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,7 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDDEN: "TOGGLE_CART_HIDDDEN",
+  ADD_ITEM: "ADD_ITEM",
 };
 
 export default CartActionTypes;
+
