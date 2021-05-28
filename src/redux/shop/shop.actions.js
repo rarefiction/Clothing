c8381846c70a3a935896aac5752751c886448417
rare <nanun93@hotmail.com>
2021-05-29 00:06:40 +0900

    removed redux-thunk from shop actions and replaced it with redux-saga and  generator functions

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,10 +1,5 @@
 import ShopActionTypes from "./shop.types";
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
-
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
@@ -19,17 +14,20 @@ export const fetchCollectionsFailure = (errorMessage) => ({
   payload: errorMessage,
 });
 
-export const fetchCollectionsStartAsync = () => {
-  return (dispatch) => {
-    const collectionRef = firestore.collection("collections");
-    dispatch(fetchCollectionsStart());
+// using redux-thunk
+// replaced by redux-saga
+//
+// export const fetchCollectionsStartAsync = () => {
+//   return (dispatch) => {
+//     const collectionRef = firestore.collection("collections");
+//     dispatch(fetchCollectionsStart());
 
-    collectionRef
-      .get()
-      .then((snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-      })
-      .catch((err) => dispatch(fetchCollectionsFailure(err.message)));
-  };
-};
+//     collectionRef
+//       .get()
+//       .then((snapshot) => {
+//         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+//         dispatch(fetchCollectionsSuccess(collectionsMap));
+//       })
+//       .catch((err) => dispatch(fetchCollectionsFailure(err.message)));
+//   };
+// };
