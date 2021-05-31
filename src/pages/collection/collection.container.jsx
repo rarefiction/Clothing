527c66e43453407a782302e4350b3ea32cb9fc31
rare <nanun93@hotmail.com>
2021-06-01 06:58:26 +1200

    updated app component, sign-in component, and sign-up component to use react-hooks

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { compose } from "redux";
 
-import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selector";
+import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionPage from "./collection.component";
 
