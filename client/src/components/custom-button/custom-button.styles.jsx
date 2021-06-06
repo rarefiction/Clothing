4900f407bc3965d345aafbdc3a172c494da4615e
rare <nanun93@hotmail.com>
2021-06-06 19:41:59 +1200

    started to implement mobile view using media queries

--- client/src/components/custom-button/custom-button.styles.jsx
+++ client/src/components/custom-button/custom-button.styles.jsx
@@ -4,7 +4,6 @@ const buttonStyles = css`
   background-color: black;
   color: white;
   border: none;
-
   &:hover {
     background-color: white;
     color: black;
@@ -16,7 +15,6 @@ const invertedButtonStyles = css`
   background-color: white;
   color: black;
   border: 1px solid black;
-
   &:hover {
     background-color: black;
     color: white;
@@ -27,7 +25,6 @@ const invertedButtonStyles = css`
 const googleSignInStyles = css`
   background-color: #4285f4;
   color: white;
-
   &:hover {
     background-color: #357ae8;
     border: none;
@@ -38,6 +35,7 @@ const getButtonStyles = (props) => {
   if (props.isGoogleSignIn) {
     return googleSignInStyles;
   }
+
   return props.inverted ? invertedButtonStyles : buttonStyles;
 };
 
@@ -55,6 +53,5 @@ export const CustomButtonContainer = styled.button`
   cursor: pointer;
   display: flex;
   justify-content: center;
-
   ${getButtonStyles}
 `;
