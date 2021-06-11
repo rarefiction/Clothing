9056209dc548f71b3fa2826c9e8bad90e04d6f9f
rare <nanun93@hotmail.com>
2021-06-11 14:46:05 +1200

    updating our code to be pwa, added serviceworker, https and updated manifest.json

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -71,6 +71,21 @@ export const convertCollectionsSnapshotToMap = (collections) => {
   }, {});
 };
 
+export const getUserCartRef = async (userId) => {
+  const cartsRef = firestore
+    .collection("carts")
+    .where("userId", "==", "userId");
+  const snapShot = await cartsRef.get();
+
+  if (snapShot.empty) {
+    const cartDocRef = firestore.collection("carts").doc();
+    await cartDocRef.set({ userId, cartItems: [] });
+    return cartDocRef;
+  } else {
+    return snapShot.docs[0].ref;
+  }
+};
+
 export const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
